8ddc2cc42d7e4ee51979ad4f17220621d6d87b49
zhouleizhao <zhouleizhao.happy@163.com>
2018-08-08 11:28:16 +0800

    update .gitignore

---
+++ DaiJia/Home/LeftMenu/ChengWeiViewController.h
@@ -0,0 +1,17 @@
+//
+//  ChengWeiViewController.h
+//  DaiJia
+//
+//  Created by GaoBingnan on 2018/7/25.
+//  Copyright © 2018年 GaoBingnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChengWeiViewController : UIViewController
+@property (nonatomic,strong)NSString * name;
+@property (nonatomic,strong)NSString * sex;
+@property (nonatomic,assign)BOOL isPerfectInfo;
+
+@property (nonatomic,copy) void(^completeBlock)();
+@end
